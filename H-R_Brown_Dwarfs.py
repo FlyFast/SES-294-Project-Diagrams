313c9a4e8884c303458441f742d85709793cac05
Keith Norton <32936931+FlyFast@users.noreply.github.com>
2023-07-09 23:26:53 -0700

    Add files via upload
    Initial version

---
+++ H-R_Brown_Dwarfs.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+print("*****************************************************************************************")
+print("This program reads in data from the Mamajek Brown Dwarf Data Excel file and plots the data")
+# Print the current working directory to the console
+print("The current working directory is: " + os.getcwd())
+# Print the current date and time to the console
+print("The current date and time is: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print("*****************************************************************************************")
+print("")
+
+# *****************************************************************************************
+# Section 1
+# Plot Teff vs logL
+# *****************************************************************************************
+
+# Switch to the directory where the data is stored
+os.chdir('c:\\Users\\Keith\Desktop\\Dev\\SES 294 H-R Diagrams')
+
+# Read the data into a Pandas DataFrame ensuring that numeric data is treated as float()
+# Use the first row as the column names
+# Only read the Teff, Peak Wavelength, logL, and Mv columns
+mamajek = pd.read_excel('Brown Dwarf Data.xlsx', sheet_name='data', header=0, usecols='B, C, D, F, I, Z')
+
+# Print the mamajek DataFrame to the console
+print("Mamajek:")
+print(mamajek)
+
+# Copy the first 92 rows of the marmajek DataFrame to a new marmajek2 DataFrame
+# This will only include the rows with complete Teff and logL data
+# If you don't filter out the rows with missing data, the plot will not work
+# because the plot function cannot handle missing data (which are strings).
+marmajek1 = mamajek.iloc[0:77, 0:4]
+marmajek1_1 = mamajek.iloc[78:91, 0:4]
+
+# Convert all of the columns, except the first, to floats
+marmajek1.iloc[:, :] = marmajek1.iloc[:, :].astype(float)
+marmajek1_1.iloc[:, :] = marmajek1_1.iloc[:, :].astype(float)
+
+# Print the type of each row and column to the console
+# print(mamajek1.dtypes)
+
+# Print the DataFrame to the console
+#print("Mamajek1:")
+#print(marmajek1)
+
+# Set the default plot size
+plt.rcParams['figure.figsize'] = [20, 12]
+
+# Create a plot with two subplots
+# The first subplot is for Teff vs logL
+# The second subplot is for Teff vs Mv
+plt.subplot(2,2,1)
+
+# Plot a scatter plot of x=Teff and y=logL
+plt.scatter(marmajek1['Teff'], marmajek1['logL'], marker='x', color='blue', label='Stars')
+plt.scatter(marmajek1_1['Teff'], marmajek1_1['logL'], marker='+', color='brown', label='Brown Dwarfs')
+# Add a legend to the plot
+plt.legend(loc='upper right')
+# Invert the x-axis
+plt.gca().invert_xaxis()
+# Label the x-axis
+plt.xlabel('Teff (K)')
+# Label the y-axis
+plt.ylabel('logL')
+# Add a title to the plot
+plt.title('H-R Diagram for Brown Dwarfs')
+# Display the plot
+# Defer this until the second subplot is added
+# plt.show()
+
+
+
+# *****************************************************************************************
+# Section 2
+# Plot J-H vs Mv
+# *****************************************************************************************
+# Copy the first 91 rows of the marmajek DataFrame to a new marmajek2 DataFrame
+# This will only include the rows with complete Teff and Mv data
+# If you don't filter out the rows with missing data, the plot will not work
+# because the plot function cannot handle missing data (which are strings).
+marmajek2 = mamajek.iloc[78:92, 4:6]
+
+print("marmajek2:")
+print(marmajek2)
+
+# Convert all of the columns, except the first, to floats
+marmajek2.iloc[:, :] = marmajek2.iloc[:, :].astype(float)
+
+# Print the type of each row and column to the console
+# print(marmajek2.dtypes)
+
+# Print the DataFrame to the console
+print(marmajek2)
+
+# Create the third subplot
+plt.subplot(2,2,2)
+
+# Plot a scatter plot of x=W1-W2 and y=Mv
+plt.scatter(marmajek2['Mv'], marmajek2['W1-W2'])
+# Invert the x-axis
+plt.gca().invert_xaxis()
+# Invert the y-axis
+#plt.gca().invert_yaxis()
+# Label the x-axis
+plt.xlabel('W1-W2 (Color)')
+# Label the y-axis
+plt.ylabel('Mv')
+# Add a title to the plot
+plt.title('Color-Magnitude Diagram for Brown Dwarfs')
+
+
+# *****************************************************************************************
+# Section 3
+# Plot  Teff vs peak wavelength
+# *****************************************************************************************
+# Copy only the rows with complete Teff and wavelength data
+# If you don't filter out the rows with missing data, the plot will not work
+# because the plot function cannot handle missing data (which are strings).
+marmajek3 = mamajek.iloc[1:78, 0:2]
+marmajek3_1= mamajek.iloc[79:117, 0:2]
+
+print("marmajek3:")
+print(marmajek3)
+
+# Convert all of the columns, except the first, to floats
+marmajek3.iloc[:, :] = marmajek3.iloc[:, :].astype(float)
+marmajek3_1.iloc[:, :] = marmajek3_1.iloc[:, :].astype(float)
+
+# Print the type of each row and column to the console
+# print(marmajek3.dtypes)
+
+# Print the DataFrame to the console
+print(marmajek3)
+
+# Create the third subplot
+plt.subplot(2,2,3)
+
+# Plot a scatter plot of x=W1-W2 and y=Mv
+plt.scatter(marmajek3['Teff'], marmajek3['Peak wavelength (μm)'], marker='x', color='blue', label='Stars') # Stars
+plt.scatter(marmajek3_1['Teff'], marmajek3_1['Peak wavelength (μm)'], marker='+', color='brown', label='Brown Dwarfs') # Brown Dwarfs
+# Add a legend to the plot
+plt.legend(loc='upper right')
+# Invert the x-axis
+#plt.gca().invert_xaxis()
+# Invert the y-axis
+#plt.gca().invert_yaxis()
+# Label the x-axis
+plt.xlabel('Teff/Spectral Class')
+# Label the y-axis
+plt.ylabel('Peak wavelength (μm)')
+# Add a title to the plot
+plt.title("Wien's Law Diagram for Brown Dwarfs")
+
+
+# Display the plot
+plt.show()
